5a197b82ec48e09f6e418aa4a9919db784cd0189
Naman Bansal <namanbansal102@gmail.com>
2026-02-17 18:45:59 +0530

    Added Template of Home Page

---
+++ frontend1/app/page.tsx
@@ -0,0 +1,10 @@
+import DemoOne from "@/components/HomePage";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+  <>
+  <DemoOne></DemoOne>
+  </>
+  );
+}
---
+++ frontend1/components/HomePage.tsx
@@ -0,0 +1,7 @@
+import AetherFlowHero from "@/components/ui/aether-flow-hero";
+
+export default function DemoOne() {
+  return  <main className="App bg-black">
+      <AetherFlowHero />
+    </main>
+}
